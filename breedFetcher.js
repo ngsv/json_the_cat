26b107fd67de97be63dc6e7711c2f90c024f3d5a
Steven Ngov <gl.steven.ngov@gmail.com>
2021-12-06 19:33:12 +0000

    Refactor and implement mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,24 @@
 const request = require("request");
-let args = process.argv.slice(2);
-let arg = args[0];
 
-request(`https://api.thecxxatapi.com/v1/breeds/search?q=${arg}`, function(error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the Google homepage.
 
-  if (!error) {
-    const data = JSON.parse(body);
-    console.log(data);
-    console.log(typeof data);
-
-    if (data.length === 0) {
-      console.log("The requested breed was not found.");
+    if (error) {
+      callback("We got an error! Check your URL and try again.", null);
     } else {
-      console.log(data[0]["description"]);
+      const data = JSON.parse(body);
+      if (data.length !== 0) {
+        callback(null, data[0]["description"]);
+      } else {
+        callback("The requested breed was not found.", null);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
